078fed8475694dd47030858ce95d12afcff8cc87
Mint <40682150+SEI-Mint@users.noreply.github.com>
2025-12-15 00:25:45 +0800

    Create load.js

---
+++ JS/load.js
@@ -0,0 +1,51 @@
+(function (window, document) {
+    class Mint {
+        constructor(...args) {
+            const argc = args.length;
+
+            this.loaded = new Set();
+        }
+
+        timestamp() {
+            return (new Date()).getTime();
+        }
+
+        require(path) {
+            const self = this;
+            const segment = path?.match(/[^\.]+/g);
+            const prefix = "https://raw.githubusercontent.com/SEI-Mint/Resource.SEI-Mint/refs/heads/main/";
+            const extension = ".js";
+            const module = segment[segment.length - 1];
+            const url = prefix + segment.join('/') + extension + "?v=" + self.timestamp().toString();
+
+            const response = function (e) {
+                if (e?.target?.readyState == 4 && e.target.responseText) {
+                    let eScript = document.createElement("script");
+                    eScript.setAttribute("rel", "text/javascript");
+                    eScript.setAttribute("path", path);
+                    eScript.innerHTML = e.target.responseText;
+                    document.body.append(eScript);
+                    self.loaded[path] = {
+                        [module]: url
+                    };
+                }
+            };
+
+            const request = function () {
+                const http = new XMLHttpRequest();
+                ["progress", "load", "error", "abort"].forEach(function (action, i, l) {
+                    http.addEventListener(action, response);
+                });
+                http.open("GET", url, true);
+                http.send();
+            };
+
+            if (!self.loaded.has(path)) {
+                request();
+            }
+        }
+    };
+
+    window.M = new Mint();
+    M.require('JS.mint');
+})(window, document);
